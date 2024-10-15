d58a367e240ac3f50ae2306e3158ae830eb915e2
compactspace <129174212+compactspace@users.noreply.github.com>
2024-10-15 16:34:43 +0900

    Update TestLogin.js

--- reactex01/src/pages/TestLogin.js
+++ reactex01/src/pages/TestLogin.js
@@ -16,7 +16,7 @@ export const TestLogin = () => {
         if (id != undefined && id != null) {
 
 
-            axios.get('http://localhost:4000/testlogin?id=' + id).then((res) => {
+            axios.get('https://localhost:4000/testlogin?id=' + id).then((res) => {
 
                 alert("왓더퍽")
                 if (res.data.status != -1) {
